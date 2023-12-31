6e7084564db85890fe2349298566d4a65bcc051c
unknown <industrialdesignoscar@gmail.com>
2023-12-31 17:06:44 -0500

    Fix menu and added new animation, Header V2

--- src/components/Header2/Header2.jsx
+++ src/components/Header2/Header2.jsx
@@ -28,7 +28,7 @@ const Header2 = () => {
                         hideOutline={false}
                         onClick={() => setOpen(!open)}
                         color="#33CCCC"
-            
+
                     ></Hamburger>
                 </div>
                 <div className="header-logo">
@@ -36,23 +36,22 @@ const Header2 = () => {
                     <img src={logo} alt="header-img" />
                 </div>
             </div>
-            {open === false ? null : (
-                <nav className="header-nav">
-                    <div className="header-nav__sections">
-                        <ul className="header-nav__list">
-                            {menuLinks.map((link, index) => (
-                                <li className="header-nav__elements" key={index}>
-                                    <NavLink className="header-nav__links" to={link.url} onClick={() => setOpen(false)}>
-                                        {link.name}
-                                    </NavLink>
-                                </li>
-                            ))}
-                      
-                            <NavSocial/>
-                        </ul>
-                    </div>
-                </nav>
-            )}
+
+            <nav className={`header-nav ${open ? 'slide-in' : 'slide-out'}`}>
+                <div className="header-nav__sections">
+                    <ul className="header-nav__list">
+                        {menuLinks.map((link, index) => (
+                            <li className="header-nav__elements" key={index}>
+                                <NavLink className="header-nav__links" to={link.url} onClick={() => setOpen(false)}>
+                                    {link.name}
+                                </NavLink>
+                            </li>
+                        ))}
+
+                        <NavSocial />
+                    </ul>
+                </div>
+            </nav>
         </div>
     );
 };
